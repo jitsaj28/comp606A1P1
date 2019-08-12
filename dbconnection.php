7a7411f9fe4c1634f6cfa160780103827750cbe5
jitsaj28 <52937081+jitsaj28@users.noreply.github.com>
2019-08-12 23:31:46 +1200

    Add files via upload

---
+++ dbconnection.php
@@ -0,0 +1,18 @@
+<?php
+ 
+$C_host='localhost';
+$C_user='root';
+$C_password='';
+$Dbase='Jithu';
+
+$db=mysqli_connect($C_host,$C_user,$C_password,$Dbase);
+/*
+if($db==true)
+{
+    echo'running';
+}
+else{
+    echo 'failed';
+}
+*/
+?>
